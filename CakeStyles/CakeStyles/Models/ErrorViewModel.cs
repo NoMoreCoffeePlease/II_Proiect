e9f40794438a412be3b5f07d071d869499fed700
AndreiStoica32 <andrei@foodstyles.com>
2020-03-28 14:09:06 +0200

    MVC project initial setup

---
+++ CakeStyles/CakeStyles/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CakeStyles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
